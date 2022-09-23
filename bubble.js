d313804bf652c93677caaac2e2f04d2f6b8593b5
Jordan Wong <pjwong97@gmail.com>
2022-09-23 12:12:31 -0700

    complete bubble and merge

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+  //compare first element with remaining elements, if larger, move up 1 and keep comparing
+  // i is first loop, i is end of array
+  // j is start
+  for (let i = nums.length - 1; i >= 1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+      }
+    }
+  }
+  return nums;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,38 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let sorted = [];
+  let xindex = 0;
+  let yindex = 0;
 
-function mergeSort() {}
+  while (xindex < arr1.length && yindex < arr2.length) {
+    if (arr2[yindex] < arr1[xindex]) {
+      sorted.push(arr2[yindex]);
+      yindex++;
+    } else {
+      sorted.push(arr1[xindex]);
+      xindex++;
+    }
+  }
 
-module.exports = { merge, mergeSort};
+  if (xindex === arr1.length) {
+    return sorted.concat(arr2.slice(yindex));
+  }
+
+  if (yindex === arr2.length) {
+    return sorted.concat(arr1.slice(xindex));
+  }
+
+}
+
+function mergeSort(arr) {
+  //break up array into halves until array.length 1
+  //basecase:
+  if (arr.length <= 1) return arr;
+
+  let upperhalf = Math.floor(arr.length / 2);
+
+  return merge(
+    mergeSort(arr.slice(0, upperhalf)),
+    mergeSort(arr.slice(upperhalf)));
+}
+
+module.exports = { merge, mergeSort };
